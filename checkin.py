070acbd127f1316a1458f9a40262f43e51cd8800
seazhy <96967408+seazhy@users.noreply.github.com>
2025-01-27 18:36:59 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -16,8 +16,8 @@ B64MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 s = requests.Session()
 
 # 在下面两行的引号内贴上账号（仅支持手机号）和密码
-username = ""
-password = ""
+#username = ""
+#password = ""
 
 
 def int2char(a):
